1d93fc31eb4e10021aa91bef1168a781b85a8140
Jarred Hone <tailcracker@gmail.com>
2014-08-02 14:57:22 +1200

    Initial Implementation of Envelope

--- src/game/Envelope.java
+++ src/game/Envelope.java
@@ -1,5 +1,46 @@
 package game;
 
+/** Class to hold properties of the hidden cards, acts like the envelope would in the physical game
+ * 
+ * @author Jarred Hone
+ *
+ */
 public class Envelope {
-
+	private Card murderer;
+	private Card murderWeapon;
+	private Card murderRoom;
+	
+	public Envelope(Card inputMurderer, Card inputMurderWeapon, Card inputMurderRoom){ //TODO Possibly change these to a subtype of card so that it can check there is one of each better?
+		this.murderer = inputMurderer;
+		this.murderWeapon = inputMurderWeapon;
+		this.murderRoom = inputMurderRoom;
+	}
+	
+	
+	/**
+	 * Returns the Murderer Card, Just for checking at this point.
+	 * @return Murderer Card
+	 */
+	public Card getMurderer(){
+		return murderer;
+	}
+	
+	/**
+	 * Returns the Murder Weapon, Just for checking at this point.
+	 * @return Murder Weapon Card
+	 */
+	public Card getMurderWeapon(){
+		return murderer;
+	}
+	
+	/**
+	 * Returns the Murder Room Card, Just for checking at this point
+	 * @return Murderer Card
+	 */
+	public Card getMurderRoom(){
+		return murderer;
+	}
+	
+	
+	
 }
