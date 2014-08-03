2224be2efc972dfdec5a711707809f62a37c2744
Jarred Hone <honejarred@gmail.com>
2014-08-04 10:25:58 +1200

    Weapon.java first commit plus some changes

--- src/game/Weapon.java
+++ src/game/Weapon.java
@@ -1,5 +1,22 @@
 package game;
 
+/**
+ * This class represents a physical Weapon object. 
+ * It contains an ID pertaining to the Weapon type.
+ * @author Jarred Hone
+ *
+ */
 public class Weapon {
+	private String weaponID;
+	public Weapon(String id){
+		this.weaponID = id;
 
+	}
+	/**
+	 * Method to return which weapon type this is.
+	 * @return Returns string containing weapon type id
+	 */
+	public String getWeaponType(){
+		return weaponID;
+	}
 }
