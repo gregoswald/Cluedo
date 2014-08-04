424a2199e08a517096341b16d516102b02a69861
Jarred Hone <honejarred@gmail.com>
2014-08-04 12:04:15 +1200

    Update

--- src/ui/Board.java
+++ src/ui/Board.java
@@ -1,5 +1,14 @@
 package ui;
-
+import game.Square;
+/**
+ * Class Which constitutes the board logic within the Game
+ * @author Jarred Hone
+ *
+ */
 public class Board {
+	Square[][] board;
+public Board(){
+	
+}
 
 }
