3868211ed495a11694fab4b13fed05a032c5a289
Greg Oswald <oswald.gm@gmail.com>
2014-08-15 23:07:04 +1200

    added some comments to BoardCanvas

--- src/ui/BoardCanvas.java
+++ src/ui/BoardCanvas.java
@@ -16,6 +16,7 @@ public class BoardCanvas extends JPanel{
 	private int canvas_width = 600;
 	private int canvas_height = 600;
 	private final int grid_lines = 24;
+	private final int grid_lines_v = 23;
 	private final int grid_size = (int)canvas_width / grid_lines;
 	private int square_x = 0;
 	private int square_y = 0;
@@ -48,8 +49,44 @@ public class BoardCanvas extends JPanel{
 		}
 
 	}
-	public void drawCube(int x, int y){
-
+	/**
+	 * draws a square on the board.
+	 * @param 
+	 * @param 
+	 */
+	public void drawSquare(){
+		Square[][] squares = board.getBoardArray();
+		int x = 0;
+		int y = 0;
+		while(x<squares.length){
+			while(y<squares[x].length){
+				switch(squares[x][y].getID().ordinal()){
+				case 1:
+				
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				case 10:
+				case 11:
+				case 12:
+				case 13:
+				case 14:
+				case 15:
+				case 16:
+				case 17:
+				case 18:
+				case 19:
+				case 20:
+				case 21:
+				case 22:
+				}
+			}
+		}
 	}
 	public void paint(Graphics g){
 		g.setColor(Color.GRAY);
@@ -61,16 +98,31 @@ public class BoardCanvas extends JPanel{
 		g.setColor(Color.GREEN);
 		g.fillOval(piece_x,piece_y,piece_size,piece_size);
 	}
-
+	//square selection logic stuff, needs refactoring.
+	/**
+	 * this probably needs renaming it returns the grid 
+	 * postition of a square the  mouse clicks in
+	 * @param x
+	 * @param y
+	 */
 	public void setSquare(int x,int y){
 		square_x = x/grid_size;
-		square_y = (y/grid_size)-1;
+		square_y = (y/grid_size)-1;//the -1 is a "black"magic number im not sure why but this wont work with out it.
 	}
+	/**
+	 * this also may need a rename this converts
+	 * an arbitrary x y to an x y
+	 * Relative to the grid
+	 * kinda like a snap to grid.
+	 */
 	public void setPiece(){
 		piece_size = grid_size-1;
 		piece_x = square_x*grid_size+1;
 		piece_y = square_y*grid_size+1;
 	}
+	/**
+	 * all this dose atm is that thing where the peice gets bigger when you click
+	 */
 	public void selectPiece(){
 		//is there a piece in the current square or whatever should probs go here
 		piece_x = piece_x-3;
